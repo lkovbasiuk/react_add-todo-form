9367011a064d916f1e81f51ac8bc8e121ff2b0ff
Liudmyla Kovbasiuk <lchubatiuk96@gmail.com>
2025-09-22 10:41:19 +0200

    Solution

--- src/components/TodoInfo/TodoInfo.tsx
+++ src/components/TodoInfo/TodoInfo.tsx
@@ -1,1 +1,11 @@
-export const TodoInfo = () => {};
+import cn from 'classnames';
+import { UserInfo } from '../UserInfo';
+
+export const TodoInfo = ({ todo }) => (
+  <article
+    className={cn('TodoInfo', { 'TodoInfo--completed': todo.completed })}
+  >
+    <h2 className="TodoInfo__title">{todo.title}</h2>
+    <UserInfo todo={todo} />
+  </article>
+);
--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,1 +1,18 @@
-export const UserInfo = () => {};
+import React from 'react';
+import { Todo } from '../../types';
+
+type Props = {
+  todo: Todo;
+};
+
+export const UserInfo: React.FC<Props> = ({ todo }) => {
+  if (!todo.user) {
+    return null;
+  }
+
+  return (
+    <a className="UserInfo" href={`mailto:${todo.user.email}`}>
+      {todo.user.name}
+    </a>
+  );
+};
